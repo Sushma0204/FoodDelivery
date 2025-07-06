c87a58dfda86820f6bb9ee00c2c331557e6e77bf
Sushma <98072240+Sushma0204@users.noreply.github.com>
2025-07-06 10:51:19 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -29,6 +29,7 @@ const App = () => {
             <Route path='/cart' element={<Cart />} />
             <Route path='/order' element={<PlaceOrder />} />
             <Route path='/verify' element={<Verify />} />
+            <Route path="//verify" element={<Verify />} />
             <Route path='/myorders' element={<MyOrders />} />
           </Routes>
         </div>
