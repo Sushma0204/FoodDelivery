bbddf85d13a947c50df12d156d79afc8e69f8fa1
Sushma <98072240+Sushma0204@users.noreply.github.com>
2025-07-06 10:31:28 +0530

    Update PlaceOrder.jsx

--- Frontend/src/pages/PlaceOrder.jsx
+++ Frontend/src/pages/PlaceOrder.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { StoreContext } from '../context/StoreContext'
 import axios from 'axios'
 import { useNavigate } from 'react-router'
